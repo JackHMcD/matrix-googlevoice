eef8e2e0e121037ab1f8b90a35a8362e0fec5ce4
JackHMcD <JackHMcD@gmail.com>
2023-12-20 23:54:01 -0700

    Create smtplogin.js

---
+++ smtplogin.js
@@ -0,0 +1,31 @@
+const config = require('./config.js')
+const { OAuth2Client } = require('google-auth-library');
+const Imap = require('imap');
+const base64 = require('base64-js');
+const fs = require('fs');
+
+// Replace these values with your OAuth2 credentials
+const clientId = config.clientId;
+const clientSecret = config.clientSecret;
+const refreshToken = config.refreshToken;
+
+// Create an OAuth2 client with the given credentials
+const oAuth2Client = new OAuth2Client({
+    clientId,
+    clientSecret,
+});
+
+// Set the refresh token for the client
+oAuth2Client.setCredentials({ refresh_token: refreshToken });
+
+// Function to refresh the access token
+async function refreshAccessToken() {
+    try {
+        const { token } = await oAuth2Client.getAccessToken();
+        const xoauth2 = `user=${(config.email)}\u0001auth=Bearer ${token}\u0001\u0001`;
+        const encodedToken = base64.fromByteArray(Buffer.from(xoauth2, 'utf-8'));
+
+        // Save the encoded token to a file
+        fs.writeFileSync('encodedToken.txt', encodedToken, 'utf-8');
+    }
+  refreshAccessToken();
